b6a95edc8002a621ffa2aed73470a5b24c2d5fd7
anonopiran <anonopiran@proton.me>
2023-02-01 19:58:31 +0330

    typo fix

--- Config/types.go
+++ Config/types.go
@@ -10,8 +10,8 @@ type LogLevelType string
 type UDPAddressType string
 type SettingsType struct {
 	UDPTargets    []UDPAddressType `env:"UDP_TARGETS" env-required:"true"`
-	SizeMin       float32          `env:"SIZE_Min" env-default:"500000"`
-	SizeMax       float32          `env:"SIZE_Max" env-default:"2000000"`
+	SizeMin       float32          `env:"SIZE_MIN" env-default:"500000"`
+	SizeMax       float32          `env:"SIZE_MAX" env-default:"2000000"`
 	RateLambda    float64          `env:"RATE_LAMBDA" env-default:"1"`
 	StatsInterval int              `env:"STATS_INTERVAL" env-default:"30"`
 	LogLevel      LogLevelType     `env:"LOG_LEVEL" env-default:"warning"`
